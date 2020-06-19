3dd784acb0798fc27977c561767d59f2eb64aafe
Omer Yair <omeryair@gmail.com>
2020-06-19 11:06:36 +0300

    Small bug fix in setting GruvBox color scheme

--- dotcli.sh
+++ dotcli.sh
@@ -1583,8 +1583,10 @@ run_setup_gui_stuff() {
     ## ==========================
     if [ -z "$(cat $HOME/.dir_colors/dircolors | grep gruvbox)" ]; then
         echo "-> Set colorscheme in Gnome terminal"
+        if [ -d "/tmp/gnome-terminal-colors" ]; then
+            rm -rf /tmp/gnome-terminal-colors
+        fi
         git clone https://github.com/metalelf0/gnome-terminal-colors.git /tmp/gnome-terminal-colors
-        default_terminal_id=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')
         /tmp/gnome-terminal-colors/install.sh -s gruvbox-dark -p ":$default_terminal_id" --install-dircolors
         rm -rf /tmp/gnome-terminal-colors
         gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${default_terminal_id}/ use-theme-transparency false
