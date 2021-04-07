291896f34a8d9f34fb8b57501936dac697170603
Omer Yair <omeryair@gmail.com>
2021-04-07 13:43:06 +0300

    Updated nvidia-docker installation

--- dotcli.sh
+++ dotcli.sh
@@ -1296,16 +1296,9 @@ run_install_nvidia_docker() {
     ## Run
     ## ---
     echo "-> Install nvidia-docker"
-    # If you have nvidia-docker 1.0 installed: we need to remove it and all existing GPU containers
-    sudo docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
-    sudo apt-get purge -y nvidia-docker || true
-
     # Add the package repositories
-    curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
     distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-    if [ "$distribution" == "ubuntu18.10" ]; then
-        distribution="ubuntu18.04"
-    fi
+    curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
     curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
     sudo apt-get update
 
